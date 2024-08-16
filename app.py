4d0179e0ad999938da36ec935888007d75b4c3ea
Ilyas Khallouki <khalloukielias@gmail.com>
2024-08-16 11:21:06 +0100

    Credantials to connect

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy
 
+# App's credentials
+client_id = '7006daa4e47f43cda68705c1df95bf1a'
+client_secret = 'de4f3027c8724bf3b29e98c28230514c'
+redirect_uri = 'http://localhost:8888/callback' 
+
+scope = 'user-library-read playlist-read-private'
+
+# Global sporipy instance
+sp = None
+
 def create_spotify_client():
     global sp
     try:
